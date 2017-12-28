f782d56c5d13736d6f5874c25fc186a66a344bc1
Kyle T <Ampix0@gmail.com>
2017-12-28 15:12:32 -0500

    Attempt to convert .json to enviroment variables
    Attempting to remove secret .contentful.json file and use CircleCI env variables.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,11 @@
-const ctfConfig = require('./.contentful.json')
+/* const ctfConfig = require('./.contentful.json')          ## Loading in variables from CircleCI env */
+
+const ctfConfig = {
+  CTF_SPACE_ID: process.env.CTF_SPACE_ID,
+  CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
+  CTF_PERSON_ID: process.env.CTF_PERSON_ID,
+  CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID
+}
 
 const {createClient} = require('./plugins/contentful')
 const cdaClient = createClient(ctfConfig)
@@ -9,14 +16,16 @@ const cmaClient = cmaContentful.createClient({
 
 module.exports = {
   /*
-  ** Contentful
+  ** ## Contentful
   */
+
   env: {
     CTF_SPACE_ID: ctfConfig.CTF_SPACE_ID,
     CTF_CDA_ACCESS_TOKEN: ctfConfig.CTF_CDA_ACCESS_TOKEN,
     CTF_PERSON_ID: ctfConfig.CTF_PERSON_ID,
     CTF_BLOG_POST_TYPE_ID: ctfConfig.CTF_BLOG_POST_TYPE_ID
   },
+
   modules: [
     ['nuxt-sass-resources-loader', './assets/scss/vars.scss']
   ],
